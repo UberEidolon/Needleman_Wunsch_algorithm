501b48066ba657a7d1b1af283b4a750a16b34dd9
astorcode <albertabashin2005@gmail.com>
2023-12-14 21:36:12 +0300

    Fixed mutant_generator

--- mutant_generator.py
+++ mutant_generator.py
@@ -1,22 +1,37 @@
 from random import randrange, random
 
 
+# This class generates 2 similar sequences with some mutant differences for the basic algorithm
+
 class Rand_seqs_pair:
-    def __init__(self, length: int) -> None:
-        self.length = length
-        self.letters = 'ACTG'
+	def __init__(self, length: int = 64, repl_chance: float = 0.05, ins_chance: float = 0.05, del_chance: float = 0.05) -> None:
+		self.length = length
+		self.letters = 'ACTG'
+		self.repl_chance = repl_chance
+		self.ins_chance = ins_chance
+		self.del_chance = del_chance
 
-    def generate(self) -> tuple:
-        rand_letter = str((self.letters[randrange(4)]))
-        seq1: str = str(rand_letter for i in range(self.length))
-        seq2 = seq1
-        del_chance = True if random() <= 0.05 else False
-        ins_chance = True if random() <= 0.05 else False
-        seq2 = (
-            (
-                seq2[n].replace(i, rand_letter) if (random() <= 0.05) else None
-             ) for n, i in enumerate(seq2)
-            )
+		# We define chances of replacement, inseriton and deletion mutations in class arguments. Default is 0.05
+
+	def generate_seqs_pair(self) -> tuple:
+		def rand_letter():
+			return str((self.letters[randrange(4)]))
+		
+		seq1: list = list(rand_letter() for i in range(self.length))
+		seq2 = list(i for i in seq1)
+		for n, i in enumerate(seq2):
+			if random() <= self.repl_chance:
+				seq2[n] = rand_letter()
+
+			if random() <= self.ins_chance:
+				seq2[n] = (rand_letter() + i)
+
+			if random() <= self.del_chance:
+				seq2[n] = ''
 
 
-        return (seq1, seq2)
+		return (seq1, seq2)
+	
+
+# pair = Rand_seqs_pair().generate_seqs_pair()
+# print(''.join(i for i in pair[0]), ''.join(i for i in pair[1]), sep='\n')
