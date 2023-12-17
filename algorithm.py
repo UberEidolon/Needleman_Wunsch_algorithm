a98be5596199bfdecb7c3be08be844cd2d68fb73
astorcode <albertabashin2005@gmail.com>
2023-12-17 05:37:54 +0300

    Fixed the way of filling matrix body. Needs to write a level out system

--- algorithm.py
+++ algorithm.py
@@ -1,69 +1,85 @@
 from pandas import DataFrame
 import numpy
 from datetime import datetime
-
-
-# Pandas DataFrame table is using for view algorithm's matrix
-# Numpy is using for work with matrix data
-start = datetime.now()
+from mutant_generator import Rand_seqs_pair
 
 
 class Matrix:
-    def __init__(self, seq1: str, seq2: str, match: int, dismatch:int, insertion: int) -> None:
-        self.seq1 = ' ' + seq1
-        self.seq2 = ' ' + seq2
+    def __init__(self, match: int, dismatch:int, insertion: int, seqs: tuple) -> None:
+        self.seqs: tuple = tuple(list(' ' + _) for _ in seqs)
         self.match = match
-        self.dismatch = dismatch
-        self.insertion = insertion
+        self.dismatch = dismatch if dismatch <= 0 else dismatch * -1
+        self.insertion = insertion if insertion <= 0 else insertion * -1
         self.matrix = numpy.zeros(
-            shape=(len(self.seq1), len(self.seq2)), dtype="int"
-            )
+            shape=(tuple(len(_) for _ in self.seqs)),
+            dtype="int"
+        )
 
-    def __str__(self):
+    def __str__(self) -> str:
+        self.level_out()
+        # print(''.join(str(''.join(j for j in i) + '\n') for i in self.seqs))
+        
+        return ''.join(str(''.join(j for j in i) + '\n') for i in self.level_out())
+    
+    def create_table(self) -> str:
         return str(DataFrame(
             data=self.matrix,
-            index=list(self.seq1),
-            columns=list(self.seq2)
+            index=list(self.seqs[0]),
+            columns=list(self.seqs[1])
             ))
 
-    def algorithm(self):
-        def first_fill():
-            for i in range(1, len(self.seq2)):
+    def algotithm(self):
+        def head_fill():
+            for i in range(1, len(self.seqs[1])):
                 self.matrix[0, i] = self.matrix[0, i-1] + self.insertion
 
-            for i in range(1, len(self.seq1)):
+            for i in range(1, len(self.seqs[0])):
                 self.matrix[i, 0] = self.matrix[i-1, 0] + self.insertion
 
-            
-        def body():
-            row, column = 1, 1
+        def body_fill():
+            row = column = 1
+            seq_1 = self.seqs[0]
+            seq_2 = self.seqs[1]
             for i in self.matrix[1:, 1:]:
-                for j in i:
-                    up = self.matrix[row-1, column] + self.insertion
-                    left = self.matrix[row, column-1] + self.insertion
-                    dgnl = self.matrix[row-1, column-1]
-                    if self.seq1[row] == self.seq2[column]:
-                        dgnl += self.match
-                    else:
-                        dgnl += self.dismatch
+                for _ in i:
+                    up: int = self.matrix[row-1, column]
+                    left: int = self.matrix[row, column-1]
+                    dgnl: int = self.matrix[row-1, column-1]
 
-                    self.matrix[row, column] = max(up, left, dgnl)
+                    self.matrix[row, column] = max(
+                        up + self.insertion,
+                        left + self.insertion,
+                        dgnl + (
+                        self.match if seq_1[row] == seq_2[column] else self.dismatch
+                        )
+                    )
 
                     column += 1
                 
                 row += 1
                 column = 1
-            
-            print(table)
+        
+        head_fill()
+        body_fill()
 
-        def back(self):
-            pass
-
-
-        return first_fill(), body()
+    def level_out(self):
+        self.algotithm()
 
 
-table = Matrix(seq2="TGCAGTAG", seq1="ATCACAG", match=2, dismatch=-1, insertion=-2)
-table.algorithm()
-
-print(datetime.now() - start)
+if __name__ == "__main__":
+    random_table = Matrix(
+        seqs=Rand_seqs_pair().generate_seqs_pair(),
+        match=2,
+        dismatch=-1,
+        insertion=-2
+        )
+    
+    determined_table = Matrix(
+        seqs=('ATCACAG',
+              'TGCAGTAG'),
+        match=2,
+        dismatch=-1,
+        insertion=-2
+        )
+    
+    print(f"Matrix for the random sequences:\n{random_table}\nMatrix for the determined sequences:\n{determined_table}")
--- mutant_generator.py
+++ mutant_generator.py
@@ -4,7 +4,7 @@ from random import randrange, random
 # This class generates 2 similar sequences with some mutant differences for the basic algorithm
 
 class Rand_seqs_pair:
-	def __init__(self, length: int = 64, repl_chance: float = 0.05, ins_chance: float = 0.05, del_chance: float = 0.05) -> None:
+	def __init__(self, length: int = 16, repl_chance: float = 0.05, ins_chance: float = 0.05, del_chance: float = 0.05) -> None:
 		self.length = length
 		self.letters = 'ACTG'
 		self.repl_chance = repl_chance
@@ -13,11 +13,14 @@ class Rand_seqs_pair:
 
 		# We define chances of replacement, inseriton and deletion mutations in class arguments. Default is 0.05
 
+	def __str__(self) -> str:
+		return ''.join(str(''.join(j for j in i) + '\n') for i in self.generate_seqs_pair())
+
 	def generate_seqs_pair(self) -> tuple:
 		def rand_letter():
 			return str((self.letters[randrange(4)]))
 		
-		seq1: list = list(rand_letter() for i in range(self.length))
+		seq1 = list(rand_letter() for i in range(self.length))
 		seq2 = list(i for i in seq1)
 		for n, i in enumerate(seq2):
 			if random() <= self.repl_chance:
@@ -29,9 +32,9 @@ class Rand_seqs_pair:
 			if random() <= self.del_chance:
 				seq2[n] = ''
 
-
-		return (seq1, seq2)
+		return (''.join(i for i in seq1), ''.join(i for i in seq2))
 	
 
-# pair = Rand_seqs_pair().generate_seqs_pair()
-# print(''.join(i for i in pair[0]), ''.join(i for i in pair[1]), sep='\n')
+if __name__ == "__main__":
+	pair = Rand_seqs_pair()
+	print(pair)
