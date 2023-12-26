c4be28ee6b221f196f30c72c0357fe0b3d7db971
astorcode <albertabashin2005@gmail.com>
2023-12-26 22:44:50 +0300

    Done!

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from random import randrange
+
+
+print('Мой вопрос по множествам: ', randrange(1, 15))
+print('Мой вопрос по пределам: ', randrange(1, 16))
+print('Мой вопрос по диффурам: ', randrange(1, 26))
+print('Мой вопрос по интегралам: ', randrange(1, 21))
--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,5 @@
 from pandas import DataFrame
 import numpy
-from datetime import datetime
 from mutant_generator import Rand_seqs_pair
 
 
@@ -16,11 +15,9 @@ class Matrix:
         )
 
     def __str__(self) -> str:
-        self.level_out()
-        # print(''.join(str(''.join(j for j in i) + '\n') for i in self.seqs))
-        
-        return ''.join(str(''.join(j for j in i) + '\n') for i in self.level_out())
-    
+        pair = self.level_out()
+        return f'{pair[0]}\n{pair[1]}'
+
     def create_table(self) -> str:
         return str(DataFrame(
             data=self.matrix,
@@ -61,10 +58,47 @@ class Matrix:
         
         head_fill()
         body_fill()
+        return self.create_table()
 
     def level_out(self):
         self.algotithm()
+        first_str = second_str = ''
+        i = len(self.matrix) - 1
+        j = len(self.matrix[0]) - 1
+        while (i and j) != 0:
+            up = self.matrix[i][j-1]
+            left = self.matrix[i-1][j]
+            dngl = self.matrix[i-1][j-1]
+            if max(up, left, dngl) == up:
+                first_str += '-'
+                second_str += self.seqs[1][j]
+                j += -1
 
+            elif max(up, left, dngl) == left:
+                first_str += self.seqs[0][i]
+                second_str += '-'
+                i += -1
+
+            else:
+                first_str += self.seqs[0][i]
+                second_str += self.seqs[1][j]
+                i += -1
+                j += -1
+
+        first_str = list(reversed(first_str))
+        second_str = list(reversed(second_str))
+        for n, i in enumerate(first_str):
+            if i == '-':
+                first_str[n], first_str[n-1] = first_str[n-1], '-'
+
+        for n, i in enumerate(second_str):
+            if i == '-':
+                second_str[n] = second_str[n-1]
+                second_str[n-1] = '-'
+        pair = []
+        pair.append(''.join(i for i in first_str))
+        pair.append(''.join(i for i in second_str))
+        return pair
 
 if __name__ == "__main__":
     random_table = Matrix(
@@ -82,4 +116,6 @@ if __name__ == "__main__":
         insertion=-2
         )
     
-    print(f"Matrix for the random sequences:\n{random_table}\nMatrix for the determined sequences:\n{determined_table}")
+    print(random_table.algotithm())
+    print(random_table)
+    
